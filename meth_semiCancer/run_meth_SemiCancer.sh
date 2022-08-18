fb02dda1d25fa57febf23187008123613be203eb
cbi-bioinfo <cbi.bioinfo@gmail.com>
2022-08-18 21:53:29 +0900

    Update run_meth_SemiCancer.sh

--- meth_semiCancer/run_meth_SemiCancer.sh
+++ meth_semiCancer/run_meth_SemiCancer.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-train_X="../preprocessing/train_X"
-test_X="../preprocessing/test_X"
-train_Y="../preprocessing/train_Y"
-test_Y="../preprocessing/test_Y"
-unlabel_X="../preprocessing/unlabel_X"
-unlabel_Y="../preprocessing/unlabel_Y"
+train_X="../preprocessing/train_X.csv"
+test_X="../preprocessing/test_X.csv"
+train_Y="../preprocessing/train_Y.csv"
+test_Y="../preprocessing/test_Y.csv"
+unlabel_X="../preprocessing/unlabel_X.csv"
+unlabel_Y="../preprocessing/unlabel_Y.csv"
 confidence_threshold=0.0
 
 python meth_SemiCancer.py $train_X $test_X $train_Y $test_Y $unlabel_X $unlabel_Y $confidence_threshold
